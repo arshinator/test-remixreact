04dfa3cf46946da7ed575d961de218cf7016dd14
uv <utkarsh.next@gmail.com>
2024-02-07 00:28:28 +0530

    Dahsboard Homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,45 @@
-import type { MetaFunction } from "@remix-run/node";
+import ReactDOM from 'react-dom';
+import React, {useEffect, useState} from 'react';
+import '../styles/tailwind.css';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+function HomePage() {
+  const [currentDateTime, setCurrentDateTime] = useState<string>('');
 
-export default function Index() {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const formattedDateTime = now.toLocaleString('en-US',{
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute:'numeric',
+        second: 'numeric',
+        hour12: true,
+      });
+      setCurrentDateTime(formattedDateTime);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="min-h-screen flex flex-col justify-start items-start bg-gray-100 p-4">
+      <div className="w-full bg-white rounded shadow-md text-center mb-4 p-8">
+        <div className="text-4xl font-bold text-green-400 mb-4">{currentDateTime}</div>
+      </div>
+      <div className="divider divider-accent">TODAY'S TASKS</div>
+      <div className="w-full bg-white rounded shadow-md text-center p-8 text-yellow-400">
+        <div className="text-4xl font-bold mb-4">Card 2</div> 
+      </div>
+      <div className="w-full bg-white rounded shadow-md text-center p-8 mt-4 text-yellow-300">
+        <div className="text-4xl font-bold mb-4">Card 3</div>
+      </div>
+      <div className="w-full bg-white rounded shadow-md text-center p-8 mt-4 text-yellow-200">
+        <div className="text-4xl font-bold mb-4">Card 3</div>
+      </div>
     </div>
   );
 }
+
+export default HomePage;
