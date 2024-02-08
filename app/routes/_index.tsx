6a7ef28182cbf0d4b82837ba712e006229dda867
uv <utkarsh.next@gmail.com>
2024-02-08 17:29:22 +0530

    updated text size

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -27,20 +27,20 @@ function HomePage() {
   return (
     <div className="min-h-screen flex flex-col justify-start items-start bg-gray-100 p-4">
       <div className="w-full bg-white rounded shadow-md text-center mb-4 p-8">
-        <div className="text-4xl font-bold text-green-400 mb-4">{currentDateTime}</div>
+        <div className="text-8xl font-bold text-green-400 mb-4">{currentDateTime}</div>
       </div>
       <div className="divider divider-accent">TODAY'S TASKS</div>
       <div className="w-full bg-white rounded shadow-md text-center p-8 text-yellow-500">
-        <div className="text-4xl font-bold mb-4 line-through">Card 1</div> 
+        <div className="text-6xl font-bold mb-4 line-through">Card 1</div> 
       </div>
       <div className="w-full bg-white rounded shadow-md text-center p-8 mt-4 text-yellow-400">
-        <div className="text-4xl font-bold mb-4">Card 2</div>
+        <div className="text-6xl font-bold mb-4">Card 2</div>
       </div>
       <div className="w-full bg-white rounded shadow-md text-center p-8 mt-4 text-yellow-300">
-        <div className="text-4xl font-bold mb-4">Card 3</div>
+        <div className="text-6xl font-bold mb-4">Card 3</div>
       </div>
       <div className="w-full bg-white rounded shadow-md text-center p-8 mt-4 text-yellow-200">
-        <div className="text-4xl font-bold mb-4">Card 4</div>
+        <div className="text-6xl font-bold mb-4">Card 4</div>
       </div>
       <FullscreenButton />
     </div>
