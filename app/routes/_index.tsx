6ea198a9bf189394a1a56a9df1ee11728060c2c2
uv <utkarsh.next@gmail.com>
2024-02-08 17:32:46 +0530

    updated text size

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -27,7 +27,7 @@ function HomePage() {
   return (
     <div className="min-h-screen flex flex-col justify-start items-start bg-gray-100 p-4">
       <div className="w-full bg-white rounded shadow-md text-center mb-4 p-8">
-        <div className="text-8xl font-bold text-green-400 mb-4">{currentDateTime}</div>
+        <div className="text-6xl font-bold text-green-400 mb-4">{currentDateTime}</div>
       </div>
       <div className="divider divider-accent">TODAY'S TASKS</div>
       <div className="w-full bg-white rounded shadow-md text-center p-8 text-yellow-500">
