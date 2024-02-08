829cb3c11ded18cc6077e28b3a8ad114f8722b66
uv <utkarsh.next@gmail.com>
2024-02-08 17:38:46 +0530

    updated time card color

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -26,8 +26,8 @@ function HomePage() {
 
   return (
     <div className="min-h-screen flex flex-col justify-start items-start bg-gray-100 p-4">
-      <div className="w-full bg-white rounded shadow-md text-center mb-4 p-8">
-        <div className="text-6xl font-bold text-green-400 mb-4">{currentDateTime}</div>
+      <div className="w-full rounded shadow-md text-center mb-4 p-8 bg-gradient-to-b from-purple-500 to-purple-300">
+        <div className="text-6xl font-bold text-white mb-4">{currentDateTime}</div>
       </div>
       <div className="divider divider-accent">TODAY'S TASKS</div>
       <div className="w-full bg-white rounded shadow-md text-center p-8 text-yellow-500">
