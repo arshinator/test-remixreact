b7a0dc65089f24051842045fdde35cc4adb244d4
uv <utkarsh.next@gmail.com>
2024-02-09 14:05:33 +0530

    removed time border

--- app/components/DateTimeCard.tsx
+++ app/components/DateTimeCard.tsx
@@ -35,9 +35,9 @@ function DateTimeCard() {
     <div className="w-full rounded shadow-md text-center mb-4 p-8 bg-gradient-to-b from-purple-500 to-purple-300 flex flex-col items-center">
         <div className="flex-grow"></div> {/* Empty space to push content to the top */}
           {/* <div className="text-6xl text-white mb-4">{formattedDate}</div> */}
-            <div className="rounded-md border border-4 border-white p-4">
+            
             <div className="text-9xl font-serif font-bold text-white flex-grow w-full text-center">{formattedTime}</div>
-        </div>
+        
     </div>
     );
 }
